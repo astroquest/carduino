b79d068bdab3d36bfccb7c02d9b412281f2edd3d
astroquest <maartenmenken98@gmail.com>
2024-02-11 16:50:59 +0100

    separated status into header

--- MotorController/MotorController.ino
+++ MotorController/MotorController.ino
@@ -1,40 +1,10 @@
-#include "PinConfig.h"
-#include "ParamConfig.h"
 #include <SoftwareSerial.h>
 
+#include "PinConfig.h"
+#include "ParamConfig.h"
+#include "Status.h"
+
 SoftwareSerial bluetooth(9, 8); // BT(TX, RX)
-
-enum class Status {
-  FORWARD,
-  BACKWARD,
-  LEFT,
-  RIGHT,
-  STOP,
-  CHECK_VOLTAGE,
-  INVALID
-};
-
-Status messageToStatus(char* message) {
-  if (strcmp(message, "forward") == 0) {
-    return Status::FORWARD;
-  }
-  else if (strcmp(message, "backward") == 0) {
-    return Status::BACKWARD;
-  }
-  else if (strcmp(message, "left") == 0) {
-    return Status::LEFT;
-  }
-  else if (strcmp(message, "right") == 0) {
-    return Status::RIGHT;
-  }
-  else if (strcmp(message, "stop") == 0) {
-    return Status::STOP;
-  }
-  else if (strcmp(message, "voltage") == 0) {
-    return Status::CHECK_VOLTAGE;
-  }
-  else return Status::INVALID;
-}
 
 void setup() {
   bluetooth.begin(9600);
